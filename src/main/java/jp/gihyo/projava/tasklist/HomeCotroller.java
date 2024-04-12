343dd5bd58fedc96bbf704cf9e5f22aec92e7390
kb8864 <yt7s858864@gmail.com>
2024-04-12 17:41:18 +0900

    HomeCotrollerにタスク一覧表示機能のエンドポイントを追加

--- src/main/java/jp/gihyo/projava/tasklist/HomeCotroller.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeCotroller.java
@@ -1,25 +1,30 @@
 package jp.gihyo.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-public class HomeCotroller {
+public class HomeCotroller{
+    record  TaskItem(String id, String task, String deadline, boolean done){}
+    private List<HomeRestController.TaskItem> taskItems = new ArrayList<>();
+
     @RequestMapping("/hello")
-    @ResponseBody
-    String hello(){
-        return  """
-                <html>
-                    <head><title>Hello</title></head>
-                    <body>
-                       <h1>Hello</h1>
-                        It works!
-                        現在時刻は%sです
-                    </body>
-                </html>
-                """.formatted(LocalDateTime.now());
+        String hello(Model model){
+        model.addAttribute("time", LocalDateTime.now());
+        return  "hello";
     }
+    @GetMapping("/list")
+        String listItems(Model model){
+        model.addAttribute("taskList", taskItems);
+        return  "home";
+    }
+
+
 }
